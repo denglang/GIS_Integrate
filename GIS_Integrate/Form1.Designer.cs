846f1bed34e73a7df2236b716cb1421865152e01
denglang <denglang36@yahoo.com>
2020-04-30 21:19:35 -0500

    Just created a new map

---
+++ GIS_Integrate/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GIS_Integrate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMap1 = new AxMapWinGIS.AxMap();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.btnExplorer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axMap1
+            // 
+            this.axMap1.Enabled = true;
+            this.axMap1.Location = new System.Drawing.Point(182, 44);
+            this.axMap1.Name = "axMap1";
+            this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+            this.axMap1.Size = new System.Drawing.Size(789, 544);
+            this.axMap1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 39);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(173, 574);
+            this.treeView1.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(994, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // btnExplorer
+            // 
+            this.btnExplorer.Location = new System.Drawing.Point(12, 628);
+            this.btnExplorer.Name = "btnExplorer";
+            this.btnExplorer.Size = new System.Drawing.Size(75, 23);
+            this.btnExplorer.TabIndex = 3;
+            this.btnExplorer.Text = "Explorer";
+            this.btnExplorer.UseVisualStyleBackColor = true;
+            this.btnExplorer.Click += new System.EventHandler(this.btnExplorer_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 654);
+            this.Controls.Add(this.btnExplorer);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.axMap1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMapWinGIS.AxMap axMap1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnExplorer;
+    }
+}
+
---
+++ GIS_Integrate/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxMapWinGIS;
+using MapWinGIS;
+
+namespace GIS_Integrate
+{
+    public partial class Form1 : Form
+    {
+        public static Dictionary<string, int> layerControl = new Dictionary<string, int>();
+        public Form1()
+        {
+            InitializeComponent();
+            //axMap1.Projection = tkMapProjection.PROJECTION_GOOGLE_MERCATOR;
+            axMap1.Projection = tkMapProjection.PROJECTION_WGS84;
+            axMap1.KnownExtents = tkKnownExtents.keUSA;
+            axMap1.CursorMode = tkCursorMode.cmPan;
+        }
+
+        private void btnExplorer_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnExplorer_Click_1(object sender, EventArgs e)
+        {
+            //string filename = @"C:\Work\GIS\data\EB Greenwood_profile_shapefile_csv.shp";
+            string filename = @"D:\shp\county.shp";
+            //string filename = @"C:\Work\GIS\data\Pangaea.png";
+            //string filename = @"C:\Work\GIS\data\StateFair_LangWithChuk_2018.jpg";
+            var fm = new FileManager();
+            if (!fm.get_IsSupported(filename))
+            {
+                MessageBox.Show("Data source isn't supported by MapWinGIS");
+            }
+            else
+            {
+                var obj = fm.Open(filename, tkFileOpenStrategy.fosAutoDetect, null);
+                if (fm.LastOpenIsSuccess)
+                {
+                    if (fm.LastOpenStrategy == tkFileOpenStrategy.fosVectorLayer)
+                    {
+                        var shapefile = obj as Shapefile;
+                        if (shapefile != null)
+                        {
+                            //MessageBox.Show("Shapefile was opened.");
+                            int handle = axMap1.AddLayer(obj, true);
+                            axMap1.ZoomToLayer(handle);
+                            //ZoomToValue(shapefile, "State_Name", "Iowa");
+
+                            if (handle != -1)
+                            {
+                                //MessageBox.Show("Layer was added to the map. Open strategy: " + fm.LastOpenStrategy.ToString());
+                                string fname = filename.Split('\\').Last();
+                                fname = fname.Remove(fname.Length - 4);  //get rid of the .shp
+                                treeView1.CheckBoxes = true;
+                                treeView1.Nodes.Add(fname);
+                                //treeView1.Nodes[1].Nodes[0].Nodes[0].Checked = true;
+                                if (layerControl.Count > 0)
+                                {
+                                    if (layerControl[fname] < 0)
+                                        layerControl.Add(fname, handle);
+                                    else layerControl.Add(fname + handle, handle);
+                                }
+                                else layerControl.Add(fname, handle);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Failed to add layer to the map: " + axMap1.get_ErrorMsg(axMap1.LastErrorCode));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var image = obj as MapWinGIS.Image;
+                        if (image != null)
+                            MessageBox.Show("Image was opened.");
+                        int handle = axMap1.AddLayer(obj, true);
+                        axMap1.ZoomToLayer(handle);
+                        if (handle != -1)
+                        {
+                            MessageBox.Show("Layer was added to the map. Open strategy: " + fm.LastOpenStrategy.ToString());
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to add layer to the map: " + axMap1.get_ErrorMsg(axMap1.LastErrorCode));
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to open datasource: " + fm.get_ErrorMsg(fm.LastErrorCode));
+                }
+            }
+        }
+    }
+}
